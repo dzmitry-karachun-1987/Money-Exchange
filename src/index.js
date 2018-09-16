3f31b6a538ae6fb62a7a361336844a2011e462c4
Dzmitry Karachun <dzmitry.karachun@bk.ru>
2018-09-16 13:38:24 +0300

    Money Exchange

--- src/index.js
+++ src/index.js
@@ -1,5 +1,37 @@
 // PLEASE DON'T change function name
 module.exports = function makeExchange(currency) {
-    // Your code goes here!
-    // Return an object containing the minimum number of coins needed to make change
+    var number = {};
+    if (currency <= 0) {
+      return {};
+    }
+    else if (currency>=10000) {
+      return {error: "You are rich, my friend! We don't have so much coins for exchange"};
+        }
+    else {
+      var h = Math.trunc(currency / 50);
+      currency = currency - h * 50;
+      if (h>0) {
+        number.H = h;
+      }
+      var q = Math.trunc(currency / 25);
+      currency = currency - q * 25;
+      if (q>0) {
+        number.Q = q;
+      }
+      var d = Math.trunc(currency / 10);
+      currency = currency - d * 10;
+      if (d>0) {
+        number.D = d;
+      }
+      var n = Math.trunc(currency / 5);
+      currency = currency - n * 5;
+      if (n>0) {
+        number.N = n;
+      }
+      var p = Math.trunc(currency / 1);
+      if (p>0) {
+         number.P = p;
+      }
+      return number;
+  }
 }
